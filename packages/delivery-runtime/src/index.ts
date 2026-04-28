a6bb7276cdb98a3c32d280a1a8349319ed1ad1a8
Zak Keown <zak.keown@outlook.com>
2026-04-28 08:10:51 -0600

    refactor(07-06): narrow delivery runtime barrel exports
    - Export only downstream delivery primitives and public types
    - Keep test-only Octokit class helper out of the package barrel

--- packages/delivery-runtime/src/index.ts
+++ packages/delivery-runtime/src/index.ts
@@ -1,4 +1,8 @@
-export * from "./octokit-client.js";
-export * from "./map-octokit-error.js";
-export * from "./preflight-fast.js";
-export * from "./preflight-full.js";
+export { buildOctokit } from "./octokit-client.js";
+export type { ProtostarOctokit } from "./octokit-client.js";
+export { mapOctokitErrorToRefusal, TOKEN_PATTERN } from "./map-octokit-error.js";
+export type { OctokitDeliveryPhase } from "./map-octokit-error.js";
+export { preflightDeliveryFast } from "./preflight-fast.js";
+export type { FastPreflightResult } from "./preflight-fast.js";
+export { FORBIDDEN_SCOPES, preflightDeliveryFull } from "./preflight-full.js";
+export type { DeliveryTarget, FullPreflightResult } from "./preflight-full.js";
